e3c74e23e2601b2ed1f1511db7a9ed47e65e0968
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-02-20 11:33:19 -0600

    change tabs back to spaces

--- src/create-branch.ts
+++ src/create-branch.ts
@@ -1,34 +1,34 @@
 import { Context } from '@actions/github/lib/context';
 
 export async function createBranch(getOctokit: any, context: Context, branch: string, sha?: string) {
-	const toolkit = getOctokit(githubToken());
-	// Sometimes branch might come in with refs/heads already
-	branch = branch.replace('refs/heads/', '');
-	const ref = `refs/heads/${branch}`;
+  const toolkit = getOctokit(githubToken());
+  // Sometimes branch might come in with refs/heads already
+  branch = branch.replace('refs/heads/', '');
+  const ref = `refs/heads/${branch}`;
 
-	// throws HttpError if branch already exists.
-	try {
-		await toolkit.rest.repos.getBranch({
-			...context.repo,
-			branch,
-		});
-	} catch (error: any) {
-		if (error.name === 'HttpError' && error.status === 404) {
-			const resp = await toolkit.rest.git.createRef({
-				ref,
-				sha: sha || context.sha,
-				...context.repo,
-			});
+  // throws HttpError if branch already exists.
+  try {
+    await toolkit.rest.repos.getBranch({
+      ...context.repo,
+      branch,
+    });
+  } catch (error: any) {
+    if (error.name === 'HttpError' && error.status === 404) {
+      const resp = await toolkit.rest.git.createRef({
+        ref,
+        sha: sha || context.sha,
+        ...context.repo,
+      });
 
-			return resp?.data?.ref === ref;
-		} else {
-			throw Error(error);
-		}
-	}
+      return resp?.data?.ref === ref;
+    } else {
+      throw Error(error);
+    }
+  }
 }
 
 function githubToken(): string {
-	const token = process.env.GITHUB_TOKEN;
-	if (!token) throw ReferenceError('No token defined in the environment variables');
-	return token;
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) throw ReferenceError('No token defined in the environment variables');
+  return token;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { getOctokit, context } from '@actions/github';
 import { createBranch } from './create-branch';
 
 async function run() {
-	try {
-		const branch = core.getInput('branch');
-		const sha = core.getInput('sha');
-		core.debug(`Creating branch ${branch}`);
-		const isCreated = await createBranch(getOctokit, context, branch, sha);
-		core.setOutput('created', Boolean(isCreated));
-	} catch (error: any) {
-		core.setFailed(error.message);
-	}
+  try {
+    const branch = core.getInput('branch');
+    const sha = core.getInput('sha');
+    core.debug(`Creating branch ${branch}`);
+    const isCreated = await createBranch(getOctokit, context, branch, sha);
+    core.setOutput('created', Boolean(isCreated));
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
 }
 run();

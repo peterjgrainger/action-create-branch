3be26809db23ef579a9930e44de154ae45bea824
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-02-20 11:04:10 -0600

    console log createRef output

--- src/create-branch.ts
+++ src/create-branch.ts
@@ -1,32 +1,32 @@
-import { Context } from "@actions/github/lib/context";
+import { Context } from '@actions/github/lib/context';
 
 export async function createBranch(getOctokit: any, context: Context, branch: string, sha?: string) {
-  const toolkit = getOctokit(githubToken());
-    // Sometimes branch might come in with refs/heads already
-    branch = branch.replace('refs/heads/', '');
-    
-    // throws HttpError if branch already exists.
-    try {
-      await toolkit.rest.repos.getBranch({
-        ...context.repo,
-        branch
-      })
-    } catch(error: any ) {
-      if(error.name === 'HttpError' && error.status === 404) {
-        await toolkit.rest.git.createRef({
-          ref: `refs/heads/${branch}`,
-          sha: sha || context.sha,
-          ...context.repo
-        })
-      } else {
-        throw Error(error)
-      }
-    }
+	const toolkit = getOctokit(githubToken());
+	// Sometimes branch might come in with refs/heads already
+	branch = branch.replace('refs/heads/', '');
+
+	// throws HttpError if branch already exists.
+	try {
+		await toolkit.rest.repos.getBranch({
+			...context.repo,
+			branch,
+		});
+	} catch (error: any) {
+		if (error.name === 'HttpError' && error.status === 404) {
+			const resp = await toolkit.rest.git.createRef({
+				ref: `refs/heads/${branch}`,
+				sha: sha || context.sha,
+				...context.repo,
+			});
+			console.log('Create Ref Response:', JSON.stringify(resp));
+		} else {
+			throw Error(error);
+		}
+	}
 }
 
 function githubToken(): string {
-  const token = process.env.GITHUB_TOKEN;
-  if (!token)
-    throw ReferenceError('No token defined in the environment variables');
-  return token;
+	const token = process.env.GITHUB_TOKEN;
+	if (!token) throw ReferenceError('No token defined in the environment variables');
+	return token;
 }
